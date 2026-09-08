21d86e8fe4877d427b8781a626ffe96c37beb0e8
Abuzar <timezonereifenservice.de@gmail.com>
2026-09-08 21:20:43 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { getCurrentUser } from "@/lib/auth/session";
 import { getDefaultProjectId } from "@/lib/projects/access";
 
 export default async function HomePage() {
+  // get the user from the session
   const user = await getCurrentUser();
   if (!user) redirect("/login");
   redirect(`/${getDefaultProjectId(user.userType)}/overview`);
